9e3ed9b4b8e615fd2488be45df9c3f958886086a
SemTenz <sem.jaeger@ziggo.nl>
2023-05-30 12:43:38 +0200

    6de

--- app/models/ExamenModel.php
+++ app/models/ExamenModel.php
@@ -29,33 +29,6 @@ class ExamenModel
         return $this->db->resultSet();
     }
 
-    public function getToegewezenExaminator($Id)
-    {
-        $sql = "SELECT       VOER.Type
-                            ,VOER.Kenteken
-                            ,VOER.Bouwjaar
-                            ,VOER.Brandstof
-                            ,TYVO.TypeVoertuig
-                            ,TYVO.RijbewijsCategorie
-
-                FROM        Examinator    AS  EXAMi
-                
-                INNER JOIN  Rijschool AS Rijs
-
-                ON          Rijs.Id = EXAMI.ExaminatorId
-                
-                INNER JOIN  Examen AS exam
-                
-                ON          exam.Examen.Id = EXAMI.id
-                
-                WHERE       VOIN.InstructeurId = $Id
-                
-                ORDER BY    TYVO.RijbewijsCategorie DESC";
-
-        $this->db->query($sql);
-        return $this->db->resultSet();
-    }
-
     public function getExamenById($Id)
     {
         $sql = "SELECT Id
